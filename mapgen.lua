70002d67af1c304ee74affa77e714f2f5566a417
Thomas Rudin <thomas@rudin.io>
2018-10-31 10:34:08 +0100

    optimized vars

--- mapgen.lua
+++ mapgen.lua
@@ -14,6 +14,15 @@ local height_params = {
 	persist = 0.5
 }
 
+-- mountain noise
+local mountain_params = {
+	offset = 0,
+	scale = 5,
+	spread = {x=256, y=256, z=256},
+	seed = 34252,
+	octaves = 3,
+	persist = 0.5
+}
 
 
 local c_base = minetest.get_content_id("default:desert_stone")
@@ -30,6 +39,13 @@ end
 local y_start = planet_mars.y_start
 local y_height = planet_mars.y_height
 
+-- perlin noise
+local height_perlin
+local mountain_perlin
+
+-- reuse maps
+local height_perlin_map = {}
+local mountain_perlin_map = {}
 
 minetest.register_on_generated(function(minp, maxp, seed)
 
@@ -45,8 +61,11 @@ minetest.register_on_generated(function(minp, maxp, seed)
 	local side_length = maxp.x - minp.x + 1 -- 80
 	local map_lengths_xyz = {x=side_length, y=side_length, z=side_length}
 
-	local height_perlin_map = minetest.get_perlin_map(height_params, map_lengths_xyz):get2dMap_flat({x=minp.x, y=minp.z})
+	height_perlin = height_perlin or minetest.get_perlin_map(height_params, map_lengths_xyz)
+	mountain_perlin = mountain_perlin or minetest.get_perlin_map(mountain_params, map_lengths_xyz)
 
+	height_perlin:get2dMap_flat({x=minp.x, y=minp.z}, height_perlin_map)
+	mountain_perlin:get2dMap_flat({x=minp.x, y=minp.z}, mountain_perlin_map)
 
 	local perlin_index = 1
 	local height_fill_factor = 0.8
@@ -56,10 +75,11 @@ minetest.register_on_generated(function(minp, maxp, seed)
 	for x=minp.x,maxp.x do
 
 		-- normalized factor from 0...1
-		local perlin_factor = math.min(1, math.abs( height_perlin_map[perlin_index] * 0.2 ) )
+		local height_perlin_factor = math.min(1, math.abs( height_perlin_map[perlin_index] * 0.2 ) )
+		--local mountain_perlin_factor = math.min(1, math.abs( mountain_perlin_map[perlin_index] * 0.2 ) )
 
 		-- weighted hill top and solid bottom
-		local abs_height = y_start + (y_height * 0.99) + (y_height * perlin_factor * 0.01)
+		local abs_height = y_start + (y_height * 0.99) + (y_height * height_perlin_factor * 0.01)
 		
 		for y=minp.y,maxp.y do
 			local index = area:index(x,y,z)
